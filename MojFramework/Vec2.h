c16d5a5326dce73c919692b5c234912835eb9e5d
ales2026 <ale.urankar@gmail.com>
2025-03-28 15:01:45 +0100

    templetizing Vec2.h

--- MojFramework/Vec2.h
+++ MojFramework/Vec2.h
@@ -1,21 +1,100 @@
 #pragma once
+#include <cmath>
 
-class Vec2
+template<typename T>
+
+class Vec2_
 {
 public:
-	Vec2() = default;
-	Vec2(float x_in, float y_in); 
-	Vec2 operator+(const Vec2& rhs) const;
-	Vec2& operator+=(const Vec2& rhs);
-	Vec2 operator*(float rhs) const;
-	Vec2& operator*=(float rhs);
-	Vec2 operator-(const Vec2& rhs) const;
-	Vec2& operator-=(const Vec2& rhs);
-	float GetLength() const;
-	float GetLengthSq() const;
-	Vec2& Normalize();
-	Vec2 GetNormalized() const;
+	Vec2_() = default;
+	Vec2_(T x_in, T y_in)
+	{
+		x = x_in;
+		y = y_in;
+	}
+	template<typename S>
+	explicit Vec2_(const Vec2_<S>& src)
+		:
+		x((T)src.x),
+		y((T)src.y)
+	{}
+	Vec2_ operator+(const Vec2_& rhs) const
+	{
+		return Vec2_(*this) += rhs;
+	}
+	Vec2_& operator+=(const Vec2_& rhs)
+	{
+		x += rhs.x;
+		y += rhs.y;
+		return *this;
+	}
+	Vec2_ operator-(const Vec2_& rhs) const
+	{
+		return Vec2_(*this) -= rhs;
+	}
+	Vec2_& operator-=(const Vec2_& rhs)
+	{
+		x -= rhs.x;
+		y -= rhs.y;
+		return *this;
+	}
+	Vec2_ operator*(T rhs) const
+	{
+		return Vec2_(*this) *= rhs;
+	}
+	Vec2_& operator*=(T rhs)
+	{
+		x *= rhs;
+		y *= rhs;
+		return *this;
+	}
+	Vec2_ operator/(T rhs) const
+	{
+		return Vec2_(*this) /= rhs;
+	}
+	Vec2_& operator/=(T rhs)
+	{
+		x /= rhs;
+		y /= rhs;
+		return *this;
+	}
+	Vec2_ operator-() const
+	{
+		return Vec2_(-x, -y);
+	}
+	T GetLength() const
+	{
+		return (T)std::sqrt(GetLengthSq());
+	}
+	T GetLengthSq() const
+	{
+		return x * x + y * y;
+	}
+	Vec2_& Normalize()
+	{
+		return *this = GetNormalized();
+	}
+	Vec2_ GetNormalized() const
+	{
+		const T len = GetLength();
+		if (len != (T)0)
+		{
+			return *this / len;
+		}
+		return *this;
+	}
+	bool operator==(const Vec2_& rhs) const
+	{
+		return x == rhs.x && y == rhs.y;
+	}
+	bool operator!=(const Vec2_& rhs) const
+	{
+		return !(*this == rhs);
+	}
 public:
-	float x;
-	float y;
+	T x;
+	T y;
 };
+
+using Vec2 = Vec2_<float>;
+using Vei2 = Vec2_<int>;
