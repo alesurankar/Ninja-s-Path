b52528b03dacc73a4640873012d84bc9986ffee8
ales2026 <ale.urankar@gmail.com>
2025-08-09 10:01:56 +0200

    enemy only hiting once per 2 second

--- MojFramework/Enemy.cpp
+++ MojFramework/Enemy.cpp
@@ -5,7 +5,8 @@ Enemy::Enemy(const Vec2& pos_in)
 	LivingEntity(pos_in, Surface("Images\\enemy64x64.bmp"), width, height, ("Config\\enemy_config.txt"), c),
 	rng(std::random_device{}()),
 	vRand(-1.0f, 1.0f)
-{}
+{
+}
 void Enemy::Update(LivingEntity& other, float dt)
 {
 	Vec2 delta = other.GetPos() - GetCenter();
@@ -13,6 +14,7 @@ void Enemy::Update(LivingEntity& other, float dt)
 	if (other.DestroyedStatus() || delta.GetLengthSq() > 40000.f)
 	{
 		dir = Vec2(vRand(rng), vRand(rng));
+		speed = 10.0f;
 	}
 	else if (delta.GetLengthSq() > 100.0f)
 	{
@@ -32,12 +34,28 @@ void Enemy::Update(LivingEntity& other, float dt)
 		{
 			dir.x += 1.0f;
 		}
+		speed = 40.0f;
 	}
 	else
 	{
 		Vec2 dir(0.0f, 0.0f);
 	}
+
+	if (hitColldownTime > 0.0f)
+	{
+		hitColldownTime -= dt;
+	}
 	ReadDirection(dir);
 	pos += dir.GetNormalized() * speed * dt;
 	animations[(int)curSequence].Update(dt);
+}
+
+float Enemy::GetHitColldown()
+{
+	return hitColldownTime;
+}
+
+void Enemy::ResetHitCooldown()
+{
+	hitColldownTime = constHitCooldownTime;
 }
